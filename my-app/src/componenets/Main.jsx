b5dede70873dd1b709b27103704158e04376685d
rgib <regangib@btinternet.com>
2025-10-21 19:57:50 +0100

    New Commit

---
+++ my-app/src/componenets/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+            
+            </ul>
+        </main>
+    )
+}
