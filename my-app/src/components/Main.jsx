bf82458d329ac123183e158713e1f63877b33bec
rgib <regangib@btinternet.com>
2025-10-22 13:15:37 +0100

    New Commit

---
+++ my-app/src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main() {
+    return (
+        <main>
+           
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+            <li>Was rleased in 2013</li>
+            <li>Was originally created by Adam Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>React powers thousands of enterprise apps including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import '../src/app.css';
 
 const root = createRoot(document.getElementById("root"))
 root.render(<App />)
