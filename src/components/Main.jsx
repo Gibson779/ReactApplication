8f9974adfd3cfccde1a6bb3e43c93f0a67c712a0
rgib <regangib@btinternet.com>
2025-10-29 10:56:38 +0000

    Main update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
            
             <h1>Fun facts about React</h1>
             <ul className="facts-list">
-            <li>Was rleased in 2013</li>
+            <li>Was released in 2013</li>
             <li>Was originally created by Adam Walke</li>
             <li>Has well over 100k stars on GitHub</li>
             <li>Is maintained by Meta</li>
